8a70629af66ce207fb11d49ae365c197c986ee60
Diana <artemida91510191@gmail.com>
2021-01-11 19:48:59 +0100

    release 0-1: 2 methods are complete + tests are passed

--- gameBoard.js
+++ gameBoard.js
@@ -1,16 +1,29 @@
 module.exports = class GameBoard {
-    constructor(terrainHelper, terrainList, size) {
-        this.terrainHelper = terrainHelper;
-        this.terrainList = terrainList;
-        this.size = size;
-    }
+  constructor(terrainHelper, terrainList, size) {
+    this.terrainHelper = terrainHelper;
+    this.terrainList = terrainList;
+    this.size = size;
+  }
 
-    terrainMap() {
-        // напишите этот метод сами
+  terrainMap() {
+    let terrainMapArray = []
+    let counter = 0
+    while (counter < this.size * this.size) {
+      let length = counter + this.size
+      terrainMapArray.push(this.terrainList.slice(counter, length).map((el) => this.terrainHelper.letterToTerrain(el)))
+      counter = length
     }
+    return terrainMapArray
+  }
 
-    difficultyOfJourney() {
-        // напишите этод метод сами
+  difficultyOfJourney(journey) {
+    let scoreFinal = 0
+    for (let i = 0; i < journey.length; i++) {
+      let numberOfArray = journey[i][0]
+      let numberOfWord = journey[i][1]
+      let a = (this.terrainMap())[numberOfArray][numberOfWord]
+      scoreFinal += this.terrainHelper.scoreDifficulty(a)
     }
+    return scoreFinal
+  }
 }
-
